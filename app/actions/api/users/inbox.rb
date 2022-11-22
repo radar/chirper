7257a6f6ba254fc5e6881fca6c914529b96f7908
Ryan Bigg <me@ryanbigg.com>
2022-11-22 20:31:32 +1100

    Pedantic namespace changes to Inbox action

--- app/actions/api/users/inbox.rb
+++ app/actions/api/users/inbox.rb
@@ -1,24 +1,25 @@
 # frozen_string_literal: true
-
 module Chirper
   module Actions
-    module API::Users
-      class Inbox < Chirper::Action
-        include Deps["activity_pub.processor"]
-        def handle(request, response)
-          params = request.params.to_h.except(:username)
-          type = params[:type]
-          case type
-          when "Create"
-            activity = ActivityPub::Create.new(params)
-          when "Follow"
-            activity = ActivityPub::Follow.new(params)
+    module API
+      module Users
+        class Inbox < Chirper::Action
+          include Deps["activity_pub.processor"]
+          def handle(request, response)
+            params = request.params.to_h.except(:username)
+            type = params[:type]
+            case type
+            when "Create"
+              activity = ActivityPub::Create.new(params)
+            when "Follow"
+              activity = ActivityPub::Follow.new(params)
+            end
+
+            processor.handle(activity)
+
+            response.format = :json
+            response.body = { ok: true }.to_json
           end
-
-          processor.handle(activity)
-
-          response.format = :json
-          response.body = { ok: true }.to_json
         end
       end
     end
